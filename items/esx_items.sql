32ab5e06ff54a98cc80f41e89d1e2f4dcbbdd45b
EJJ04 <144691106+EJJ04@users.noreply.github.com>
2025-06-09 04:51:33 +0200

    Create esx_items.sql

---
+++ items/esx_items.sql
@@ -0,0 +1,8 @@
+-- ESX Items for ejj_prison script
+-- Run this SQL to add the required items to your database
+
+INSERT INTO `items` (`name`, `label`, `weight`, `rare`, `can_remove`) VALUES
+('metal_scrap', 'Metal Scrap', 5, 0, 1),
+('wood_plank', 'Wood Plank', 8, 0, 1),
+('duct_tape', 'Duct Tape', 3, 0, 1),
+('shovel', 'Shovel', 15, 0, 1); 
