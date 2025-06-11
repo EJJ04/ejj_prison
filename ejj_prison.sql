94a93090d00086704085efb1c5a730416eb70669
EJJ04 <144691106+EJJ04@users.noreply.github.com>
2025-06-11 05:42:16 +0200

    Update ejj_prison.sql

--- ejj_prison.sql
+++ ejj_prison.sql
@@ -3,5 +3,6 @@ CREATE TABLE IF NOT EXISTS `ejj_prison` (
   `time` int(11) NOT NULL,
   `date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `inventory` json DEFAULT NULL,
+  `prison` varchar(50) DEFAULT 'bolingbroke',
   PRIMARY KEY (`identifier`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci; 
